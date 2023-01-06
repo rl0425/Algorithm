e2d6bf67d265d0794514b59ade150da489d7427f
박기찬 <rl0425@naver.com>
2023-01-06 14:53:54 +0900

    문제 업데이트

---
+++ 백준/Sliver/4949.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+
+answer = []
+
+while True:
+    inputData = []
+    ans = True
+    last = ""
+
+    an = input()
+    if an == ".\n":
+        break
+
+    for x in an:
+        if x == "(" or x == "[":
+            inputData.append(x)
+            last = x
+        elif x == ")" or x == "]":
+            if last == "(" and x == ")":
+                if len(inputData) > 0:
+                    inputData.pop()
+
+            elif last == "[" and x == "]":
+                if len(inputData) > 0:
+                    inputData.pop()
+
+            else:
+                inputData = []
+                ans = False
+                break
+
+            if len(inputData) > 0:
+                last = inputData[len(inputData)-1]
+            elif len(inputData) == 0:
+                last = ""
+
+    if len(inputData) > 0 or ans == False:
+        answer.append("no")
+    else:
+        answer.append("yes")
+
+print(*answer)
