0efd00c37272abd155ed984a9e415636bc932960
박기찬 <rl0425@naver.com>
2023-02-03 23:21:54 +0900

    문제 업데이트

--- 백준/Level 3/Sliver/18870.py
+++ 백준/Level 3/Sliver/18870.py
@@ -1,1 +1,20 @@
-a, b = map(int, input().split())
+a = int(input())
+b = list(map(int, input().split()))
+
+dictSet = {}
+answer = []
+count = 0
+
+c = list(b)
+b.sort()
+for x in b:
+    if x not in dictSet:
+        dictSet[x] = count
+        count += 1
+
+for x in c:
+    answer.append(dictSet[x])
+
+print(*answer)
+
+
