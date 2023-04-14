9a3f59d48f4c7c05e050fc577ba0fe3600e32f19
박기찬 <rl0425@naver.com>
2023-04-15 03:41:58 +0900

    문제 업데이트

---
+++ 백준/Week/Week6/11399.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = list(map(int,input().split()))
+
+sorted = sorted(b)
+num = 0
+last = 0
+
+for x in sorted:
+    num = num + x
+    last = last + num
+
+print(last)
+
+#  ?? 1분컷
