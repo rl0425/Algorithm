d0399205eb6c5bd7be8931eef594b6bac366f281
박기찬 <rl0425@naver.com>
2023-05-13 21:15:44 +0900

    문제 업데이트

--- 백준/Week/Week7/11000.py
+++ 백준/Week/Week7/11000.py
@@ -1,6 +1,20 @@
-a = int(input())
-inputList = []
+import heapq
+import sys
+input = sys.stdin.readline
 
-for x in range(a):
-    inputList.append(map(int, input().split()))
+n = int(input())
 
+list = [list(map(int, input().split())) for i in range(n)]
+list.sort()
+
+queue = []
+heapq.heappush(queue, list[0][1])
+
+for i in range(1, n):
+    if list[i][0] < queue[0]:
+        heapq.heappush(queue, list[i][1])
+    else:
+        heapq.heappop(queue)
+        heapq.heappush(queue, list[i][1])
+
+print(len(queue))
