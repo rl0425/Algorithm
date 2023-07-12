0a3d5d3e1f9dfeea514348f8ebadfd8f0932615e
parkgichan <rl0425@naver.com>
2023-07-12 20:04:35 +0900

    문제 업데이트

---
+++ 백준/Week/Week13/R16928.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n,m = map(int, input().split())
+
+board = [0] * 101
+visited = [False] * 101
+
+ladder = dict()
+snake = dict()
+
+for x in range(n):
+    a, b = map(int, input().split())
+    ladder[a] = b
+
+for x in range(m):
+    a, b = map(int, input().split())
+    snake[a] = b
+
+q = deque([1])
+
+while q:
+    print("q =", q)
+    print("board =", board)
+
+    x = q.popleft()
+
+    if x == 100:
+        print(board[x])
+        break
+    for dice in range(1,7):
+        next = x + dice
+        if next <= 100 and not visited[next]:
+            if next in ladder.keys():
+                next = ladder[next]
+            if next in snake.keys():
+                next = snake[next]
+
+            if not visited[next]:
+                visited[next] = True
+                board[next] = board[x] + 1
+                q.append(next)
