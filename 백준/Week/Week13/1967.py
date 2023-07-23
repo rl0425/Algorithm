fb9f5609765259e29fb6a6fda99cb90858b0088c
박기찬 <rl0425@naver.com>
2023-07-23 22:59:14 +0900

    문제 업데이트

--- 백준/Week/Week13/1967.py
+++ 백준/Week/Week13/1967.py
@@ -1,14 +1,19 @@
+import sys
+sys.setrecursionlimit(10 ** 9)
+input = sys.stdin.readline
+
 n = int(input())
 
 tree = {}
 
 def dfs(root, dist):
     for x in tree[root]:
-        if distance[x[0]] == -1 and x[0] != 0:
+        if distance[x[0]] == -1:
             distance[x[0]] = dist + x[1]
             dfs(x[0], dist + x[1])
 
 for _ in range(n-1):
+    # dict 생성
     s, e, d = map(int, input().split())
 
     if s not in tree:
@@ -25,11 +30,15 @@ if n == 1:
     print(0)
 
 else:
+    # 첫번째 거리 구하기
     distance = [-1] * (n+1)
     distance[1] = 0
     dfs(1, 0)
 
+    # 가장 긴 인덱스 설정
     maxIdx = distance.index(max(distance))
+
+    # 두번째 거리 구하기
     distance = [-1] * (n+1)
     distance[maxIdx] = 0
 
