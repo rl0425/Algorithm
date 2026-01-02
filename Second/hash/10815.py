92def05cedc822e45f61717507a98daca26a9255
박기찬 <rl0425@naver.com>
2026-01-02 23:55:56 +0900

    feat: Add New Problem-Solving files

--- Second/hash/10815.py
+++ Second/hash/10815.py
@@ -1,15 +1,31 @@
-n = int(input())
-has_card = list(map(int, input().split()))
-m = int(input())
-crt_card = list(map(int, input().split()))
+import heapq
 
-arr = [0] * 20000001
+def dijkstra(start, end):
+    q = []
+    distance = [float('inf')] * 100001
+    distance[start] = 0
+    heapq.heappush(q, (0, start))
 
-for e in has_card:
-    arr[e] = 1
+    while q:
+        dist, now = heapq.heappop(q)
 
-for e in crt_card:
-    if arr[e] == 1:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+        if now == end:  # 도착점에 도달하면 종료
+            return dist
+
+        if distance[now] < dist:
+            continue
+
+        # 순간이동 (0초)
+        if now * 2 <= 100000 and distance[now * 2] > dist:
+            distance[now * 2] = dist
+            heapq.heappush(q, (dist, now * 2))
+
+        # 걷기 (1초)
+        for next_pos in [now - 1, now + 1]:
+            if 0 <= next_pos <= 100000 and distance[next_pos] > dist + 1:
+                distance[next_pos] = dist + 1
+                heapq.heappush(q, (dist + 1, next_pos))
+
+
+n, k = map(int, input().split())
+print(dijkstra(n, k))
